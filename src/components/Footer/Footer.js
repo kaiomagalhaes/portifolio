c76b5ae77f663360f45b616e7578bce91d3616fd
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-02-07 23:39:21 -0200

    small fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Footer.scss';
-import Link from '../Link';
 
 class Footer extends Component {
 
